530ad9be2dfea24e585533bf789d1c875cbd006a
Ant <fedora@mail.com>
2021-01-15 13:38:46 +0500

    create comment in blog

--- core_cv/blog/urls.py
+++ core_cv/blog/urls.py
@@ -5,8 +5,8 @@ app_name = 'blog'
 
 
 urlpatterns = [
-    # path("", views.BlogListView.as_view(), name="blog_list"),
-    path("blog/", views.ListOfBlogs.as_view(), name="blog_list"),
-    path("blog/<int:pk>/", views.DetailBlog.as_view(), name="blog_detail"),
+    path("blog/", views.ListOfPosts.as_view(), name="blog_list"),
+    path("blog/<int:pk>/", views.DetailPost.as_view(), name="blog_detail"),
+    path("create_blog/", views.CreatePost.as_view(), name="blog_create"),
 
 ]
--- core_cv/blog/views.py
+++ core_cv/blog/views.py
@@ -2,33 +2,44 @@ from django.shortcuts import render
 from django.views.generic import View
 from django.views import generic
 from . import models
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib import messages
 
 
-class BlogListView(View):
-    def get(self, *args, **kwarg):
-
-        context = {
-            'title': 'Blog',
-        }
-
-        return render(self.request, 'blog_list.html')
-
-
-class ListOfBlogs(generic.ListView):
+class ListOfPosts(generic.ListView):
     model = models.Blog
     context_object_name = 'blogs_list'
     template_name = "blog_list.html"
+    paginate_by = 3
 
     def get_queryset(self):
         return models.Blog.objects.all()
 
     def get_context_data(self, **kwargs):
-        context = super(ListOfBlogs, self).get_context_data(**kwargs)
+        context = super(ListOfPosts, self).get_context_data(**kwargs)
         context['title'] = 'Blog'
         return context
 
 
-class DetailBlog(generic.detail.DetailView):
+class CreatePost(LoginRequiredMixin, generic.CreateView):
+    model = models.Blog
+    template_name = "create_blog_mess.html"
+    fields = [
+        'title',
+        'content',
+        'image',
+    ]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+
+        messages.success(
+            self.request, f'Post created for {self.request.user.username}')
+
+        return super().form_valid(form)
+
+
+class DetailPost(generic.detail.DetailView):
     model = models.Blog
     template_name = "blog_detail.html"
     context_object_name = 'blog'
